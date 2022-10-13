a1f56de9265d3b3cf0bb3ebccc8ce11f8a0b5a56
sanchukanirupama <sanchukanirupama@gmail.com>
2022-10-13 10:49:57 +0530

    server changes

--- server.py
+++ server.py
@@ -15,20 +15,29 @@ def respond():
 @app.route('/result', methods=['POST'])
 
 def predict():
-    data = request.json
-    query_df = pd.DataFrame(data, index=[0])
+    try:
+        data = request.json
+        query_df = pd.DataFrame(data, index=[0])
 
-    prediction = model.predict(query_df)
-    prediction_flatten = np.hstack(prediction)
+        prediction = model.predict(query_df)
+        prediction_flatten = np.hstack(prediction)
 
-    x = prediction_flatten.tolist()[0]
-    y = prediction_flatten.tolist()[1]
+        x = prediction_flatten.tolist()[0]
+        y = prediction_flatten.tolist()[1]
 
-    data['x'] = x
-    data['y'] = y
+        data['x'] = x
+        data['y'] = y
 
-    print(data)
-    return data
+        print (data)
+        return data
+
+    except Exception as e:
+        print(e)
+        print('JSON Structure is wrong')
+        return request.json
+        
+
+    
 
 if __name__ == '__main__' :
     app.run(debug=True)
